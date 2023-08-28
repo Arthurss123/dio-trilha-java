ba7dcebcf5a7ff53ff21f9d4086fb72f902ffb34
Arthur Moura <120514370+Arthurss123@users.noreply.github.com>
2023-08-28 00:02:16 -0300

    Final do projeto ContaBanco

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+    System.out.println("Digite o seu nome completo");
+    String nomeCompleto = scanner.next();
+
+    System.out.println("digite sua agência");
+    String numeroAgencia = scanner.next();
+
+    System.out.println("Digite o numero da sua conta");
+    String numeroConta = scanner.next();
+
+    double saldo = 237.48;
+
+    System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " " + "e seu saldo" + saldo + " " + "já esta disponivel para saque." );
+
+
+
     }
 }
